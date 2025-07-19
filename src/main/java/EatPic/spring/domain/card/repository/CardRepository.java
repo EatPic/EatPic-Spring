822e8bd9ea1248f20224ebc55367bc639c3ad8f4
ian <hope030327@naver.com>
2025-07-19 13:01:52 +0900

    Refactor: 커서 페이징 Slice 수정

--- src/main/java/EatPic/spring/domain/card/repository/CardRepository.java
+++ src/main/java/EatPic/spring/domain/card/repository/CardRepository.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +15,10 @@ public interface CardRepository extends JpaRepository<Card, Long> {
   List<Card> findByUserIdAndCreatedAtBetween(Long userId, LocalDateTime start, LocalDateTime end);
 
   // 커서 페이징으로 전체 카드 조회
-  @Query("SELECT c FROM Card c WHERE c.id > :cursor ORDER BY c.id ASC")
-  List<Card> findByCursor(@Param("cursor") Long cursor, Pageable pageable);
+  @Query("""
+    SELECT c FROM Card c
+    WHERE (:cursor IS NULL OR c.id > :cursor)
+    ORDER BY c.id ASC
+""")
+  Slice<Card> findByCursor(@Param("cursor") Long cursor, Pageable pageable);
 }
