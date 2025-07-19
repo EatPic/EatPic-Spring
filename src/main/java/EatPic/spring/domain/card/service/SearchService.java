6b7a37210afbdc5f9fca5e8d99a17ac09eae55f8
ian <hope030327@naver.com>
2025-07-19 13:01:52 +0900

    Refactor: 커서 페이징 Slice 수정

--- src/main/java/EatPic/spring/domain/card/service/SearchService.java
+++ src/main/java/EatPic/spring/domain/card/service/SearchService.java
@@ -3,5 +3,5 @@ package EatPic.spring.domain.card.service;
 import EatPic.spring.domain.card.dto.response.SearchResponseDTO;
 
 public interface SearchService {
-    public abstract SearchResponseDTO.GetCardListResponseDto getAllCards(int limit, Long cursor);
+    SearchResponseDTO.GetCardListResponseDto getAllCards(int limit, Long cursor);
 }
