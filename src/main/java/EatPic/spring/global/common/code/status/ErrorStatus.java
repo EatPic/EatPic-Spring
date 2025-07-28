96df44e28079da891539d158065d323e874baa16
junho <2171168@hansung.ac.kr>
2025-07-29 01:24:41 +0900

    Feat: 커뮤니티 피드 조회하기

--- src/main/java/EatPic/spring/global/common/code/status/ErrorStatus.java
+++ src/main/java/EatPic/spring/global/common/code/status/ErrorStatus.java
@@ -39,7 +39,7 @@ public enum ErrorStatus implements BaseErrorCode {
     // 같은 날짜에 같은 meal 중복 에러
     DUPLICATE_MEAL_CARD(HttpStatus.CONFLICT, "CARD_002", "이미 같은 날짜와 같은 식사 유형의 카드가 존재합니다."),
     CARD_UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, "CARD_003", "해당 카드를 수정할 수 있는 권한이 없습니다."),
-
+    NO_RECENT_CARDS(HttpStatus.NOT_FOUND,"CARD_004","최근 7일간 작성된 피드가 없습니다,"),
 
     // 댓글 관련 응답
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT_001", "해당 댓글은 존재하지 않는 댓글입니다."),
