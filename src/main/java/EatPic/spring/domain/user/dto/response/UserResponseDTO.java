65ec9d9a177b0586cc0b81303de20c075cfafd0a
Ian <145183497+2anizirong@users.noreply.github.com>
2025-07-30 01:33:04 +0900

    Revert "Feat: 프로필 화면 API"

--- src/main/java/EatPic/spring/domain/user/dto/response/UserResponseDTO.java
+++ src/main/java/EatPic/spring/domain/user/dto/response/UserResponseDTO.java
@@ -13,19 +13,6 @@ public class UserResponseDTO {
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class ProfileIconDto {
-        private Long userId;
-        private String profileImageUrl;
-        private String nameId;
-        private String nickname;
-        private Boolean isFollowing;
-        private String introduce;
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
     public static class ProfileDto {
         private Long userId;
         private String profileImageUrl;
@@ -38,28 +25,11 @@ public class UserResponseDTO {
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class DetailProfileDto {
-        private Long userId;
-        private String profileImageUrl;
-        private String nameId;
-        private String nickname;
-        private Boolean isFollowing;
-        private String introduce;
-        private Long totalCard;
-        private Long totalFollower;
-        private Long totalFollowing;
-    }
-
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
     public static class UserIconListResponseDto{
         private int page;
         private int size;
         private int total;
-        private List<ProfileIconDto> userIconList;
+        private List<ProfileDto> userIconList;
     }
 
     @Getter
