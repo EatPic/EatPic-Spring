6ac754eabd8003b62b2d2161bdb5c839a53459d7
예진 박 <a2846461@gmail.com>
2025-07-30 23:19:15 +0900

    Feat: Role 추가 및 User, 예외처리 수정 (#5)

--- src/main/java/EatPic/spring/global/common/exception/GeneralException.java
+++ src/main/java/EatPic/spring/global/common/exception/GeneralException.java
@@ -4,6 +4,13 @@ import EatPic.spring.global.common.code.BaseErrorCode;
 import EatPic.spring.global.common.code.ErrorReasonDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @AllArgsConstructor
