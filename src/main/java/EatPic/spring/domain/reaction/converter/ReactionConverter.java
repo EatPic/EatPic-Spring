9ff2c2cc14e6678d9ed97c9f85a9af540ef943b8
junho <2171168@hansung.ac.kr>
2025-08-11 00:21:38 +0900

    feat: 반응 api 응답 'status' 추가

--- src/main/java/EatPic/spring/domain/reaction/converter/ReactionConverter.java
+++ src/main/java/EatPic/spring/domain/reaction/converter/ReactionConverter.java
@@ -4,11 +4,12 @@ import EatPic.spring.domain.reaction.dto.ReactionResponseDTO;
 import EatPic.spring.domain.reaction.entity.Reaction;
 
 public class ReactionConverter {
-    public static ReactionResponseDTO.ReactionHandleResponseDto reactionToReactionHandleResponseDTO(Reaction reaction) {
+    public static ReactionResponseDTO.ReactionHandleResponseDto reactionToReactionHandleResponseDTO(Reaction reaction, String status) {
         return ReactionResponseDTO.ReactionHandleResponseDto.builder()
                 .cardId(reaction.getCard().getId())
                 .userId(reaction.getUser().getId())
                 .reactionType(reaction.getReactionType())
+                .status(status)
                 .build();
     }
 
--- src/main/java/EatPic/spring/domain/reaction/service/ReactionService.java
+++ src/main/java/EatPic/spring/domain/reaction/service/ReactionService.java
@@ -5,6 +5,6 @@ import EatPic.spring.domain.reaction.entity.Reaction;
 import EatPic.spring.domain.reaction.entity.ReactionType;
 
 public interface ReactionService {
-    Reaction handleReaction(Long cardId, ReactionType reactionType);
+    ReactionResponseDTO.ReactionHandleResponseDto handleReaction(Long cardId, ReactionType reactionType);
     ReactionResponseDTO.CardReactionUserListDto getCardUsersByReactionType(Long cardId, ReactionType reactionType, Integer page, Integer size);
 }
