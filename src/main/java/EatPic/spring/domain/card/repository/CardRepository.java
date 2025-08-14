1324541e9255ce45d279e44874aefe30a80cb0bd
junho <2171168@hansung.ac.kr>
2025-08-14 16:43:44 +0900

    주석 삭제(이전쿼리문)

--- src/main/java/EatPic/spring/domain/card/repository/CardRepository.java
+++ src/main/java/EatPic/spring/domain/card/repository/CardRepository.java
@@ -33,20 +33,6 @@ public interface CardRepository extends JpaRepository<Card, Long> {
 
   List<Card> findAllByUserAndCreatedAtBetween(User user, LocalDateTime start, LocalDateTime end);
 
-//  @Query("""
-//SELECT c
-//FROM Card c
-//WHERE c.isDeleted = false
-//  AND c.isShared = true
-//  AND c.user.id <> :loginUserId
-//  AND c.user.id NOT IN (
-//      SELECT ub.blockedUser.id
-//      FROM UserBlock ub
-//      WHERE ub.user.id = :loginUserId
-//  )
-//  AND (:cursor IS NULL OR c.id < :cursor)
-//ORDER BY c.id DESC
-//""")
 @Query("""
 SELECT DISTINCT c
 FROM Card c
